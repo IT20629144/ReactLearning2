501aa89346a9eabf04d963ebd48a10f3c1a0d9b1
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-07-30 11:07:12 +0530

    fetchDataUseEffectPar1(dataFetchin)

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import IntervalClassCounter from './ComponentHook/IntervalClassCounter';
 import IntervalHookCounter from './ComponentHook/IntervalHookCounter';
+import DataFetching from './ComponentHook/DataFetching';
 
 function App() {
   return (
     <div className="App">
        {/* <IntervalClassCounter/> */}
-       <IntervalHookCounter/>
-
+       {/* <IntervalHookCounter/> */}
+        <DataFetching/>
     </div>
   );
 }
